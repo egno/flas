454581a02ca4c828b48ed79e386516b837d6df3b
Alexander Shelemetyev <shelemetyev@e2billing.ru>
2016-06-16 12:31:42 +0600

    err

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response, HTTP_PROVIDERS} from '@angular/http';
 import 'rxjs/Rx';
 import { Observable }     from 'rxjs/Observable';
+import { ConfigService }     from '../config/config.service';
 
 
 @Component({
   selector: 'about',
   templateUrl: 'app/about/about.component.html',
-  providers: [HTTP_PROVIDERS],
+  providers: [HTTP_PROVIDERS, ConfigService],
 })
 
 export class AboutComponent implements OnInit {
+  config: any;
 	version: string;
-    constructor(private http: Http) { } 
+  errorMessage: string;
+    constructor(private http: Http
+      //private configService: ConfigService 
+      ) { } 
 
     ngOnInit() {
         this.http.get('../../package.json')
@@ -20,6 +25,13 @@ export class AboutComponent implements OnInit {
             .subscribe((data) => {this.version=data.version},
             err=>console.log(err),
             ()=>console.log('version: ', this.version));
+        
+       // this.config = this.configService
+        //  .getConfig()
+        //  .subscribe(
+        //    data => {console.log(data)},            error => this.errorMessage = <any>error
+        //    );
+  
     }
 
     getVersion() {
