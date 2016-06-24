8ea35ab51d92c775631c5e720b70d07d81181917
Egno <glaf@mail.ru>
2016-06-24 23:44:38 +0600

    routing

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component }	from '@angular/core';
 import { ROUTER_DIRECTIVES }  from '@angular/router';
 
-import { TestComponent }	from '../test/test.component';
-import { Test1Component }	from '../test1/test1.component';
-
 import {routes} from '../app/app.routes';
 
 @Component({
     selector: 'flap',
     templateUrl: 'app/app/app.component.html', 
-    directives: [ROUTER_DIRECTIVES, TestComponent],
+    directives: [ROUTER_DIRECTIVES],
 })
 
 export class AppComponent { 
