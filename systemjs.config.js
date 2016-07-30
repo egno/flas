a68649f7d5085f31c7c9f3ae38cba20f60e69a08
Egno <glaf@mail.ru>
2016-07-30 13:56:20 +0700

    add info about author

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,7 +9,6 @@
     'app':                        'app', // 'dist',
 
     '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
 
@@ -17,7 +16,6 @@
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
 
   var ngPackageNames = [
