7dd6aa76c549704908f4130a764170c23abefa43
Alexander Shelemetyev <shelemetyev@e2billing.ru>
2016-07-31 09:12:03 +0700

    fix license

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# FLAS - free libre accounting system
+# FLAP - free libre accounting platform
 
+## English
 Web client is based on the Angular 2 demo and it use a FLAP - free libre accounting platform
+
+See LICENSE file, please!
+
+## Russian - Русский
+Web клиент Свободной учётной платформы (FLAP) на фреймворке Angular 2.
+
+Распространяеется на условиях лицензии GNU GPL v3.
+
+Более подробно о платформе читайте в [Wiki](https://github.com/egno/flas/wiki)
--- app/entity/entity.component.ts
+++ app/entity/entity.component.ts
@@ -1,3 +1,42 @@
+/**
+ * @license
+ * Copyright Alexander Shelemetyev (Александр Шелеметьев) aka Egno.
+ * https://github.com/egno/flas
+ *
+ ***** EN *****
+ * This file is part of FLAP.
+ *
+ * FLAP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, or
+ * (at your option) any later version.
+ *
+ * FLAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FLAP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Use of this source code is governed by an GNU GPLv3 license that can be
+ * found in the LICENSE file at https://github.com/egno/flas/blob/master/LICENSE
+ *
+ ***** RU *****
+ *  Этот файл — часть FLAP.
+ *
+ *  FLAP - свободная программа: вы можете перераспространять ее и/или
+ *  изменять ее на условиях Стандартной общественной лицензии GNU версии 3,
+ *  либо (по вашему выбору) любой более поздней версии этой лицензии.
+ *
+ *  FLAP распространяется в надежде, что она будет полезной,
+ *  но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+ *  или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+ *  общественной лицензии GNU.
+ *
+ *  Вы должны были получить копию Стандартной общественной лицензии GNU
+ *  вместе с этой программой. Если это не так, см.
+ *  <http://www.gnu.org/licenses/>.
+ */
 import { Component, Input, OnInit, AfterContentInit, OnDestroy } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute }       from '@angular/router';
 import { NgForm }    from '@angular/common';
